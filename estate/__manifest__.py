a751645922a9538a2bf2985d71b809f14efda6a1
Jérémie VAN BRABANT <jvanbrabant@helb-prigogine.be>
2022-12-07 11:18:45 +0100

    post update manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "helb_project",
+    'name': "estate",
     'summary': """Project for HELB Student""",
     'author': "My Company",
     'website': "http://www.yourcompany.com",
     'category': 'Uncategorized',
-    'version': '15.0.0.1',
+    'version': '16.0.0.1',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
     'data': [
     ]
