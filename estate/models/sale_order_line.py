e1a9c8d85b63c52b8f38dde6697aa837232448db
Jérémie VAN BRABANT <jvanbrabant@helb-prigogine.be>
2022-12-21 16:36:36 +0100

    testevent

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -4,3 +4,17 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
     employee = fields.Many2one("hr.employee", string="employee")
+
+    def inherited_action(self):
+        self.env["calendar.event.model"].create(
+            {
+                "name": "Test",
+                "line_ids": [
+                    Command.create({
+                        "field_1": "value_1",
+                        "field_2": "value_2",
+                    })
+                ],
+            }
+        )
+        return super().inherited_action()
