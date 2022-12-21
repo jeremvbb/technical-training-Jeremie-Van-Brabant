c7a9a0390cdbb1f8116abf55b19ebf4d147e4bfb
Jérémie VAN BRABANT <jvanbrabant@helb-prigogine.be>
2022-12-21 14:21:32 +0100

    add training date

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,6 @@
     'version': '16.0.0.1',
     'license': 'LGPL-3',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
-    'data': [
+    'data': ["views/res_groups.xml", "views/sale_order.xml"
     ]
 }
--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,3 +2,4 @@ from odoo import fields, models, Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+    training_date = fields.Date(string="Training Date")
