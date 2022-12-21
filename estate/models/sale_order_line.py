61fa25bee87222dcbe479987fd13be7b66806474
Jérémie VAN BRABANT <jvanbrabant@helb-prigogine.be>
2022-12-21 15:23:22 +0100

    test4

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,4 +3,4 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
-    employee = fields.Char(related="Employee.display_name", string="Employee", readonly=True, store=True)
+    employee = fields.Char(related="employee_id.display_name", string="Employee", readonly=True, store=True, depends=['Employee.employee_id'])
